a1927f860909e8b05e89a3836bf1f94878f2182b
arjun259194 <arjun259194@gmail.com>
2023-08-31 22:10:36 +0530

    improved error handling and added error messages

--- src/lib.rs
+++ src/lib.rs
@@ -11,16 +11,22 @@ use structopt::StructOpt;
 pub struct Args {
     #[structopt(parse(from_os_str))]
     pub dir: PathBuf,
+
+    #[structopt(short = "d", long = "depth")]
+    pub depth: bool,
 }
 
 fn get_extension_map() -> HashMap<String, String> {
     let extensions: Vec<(&str, &str)> = vec![
         ("png", "Image"),
+        ("webp", "Image"),
+        ("jfif", "Image"),
         ("jpg", "Image"),
         ("jpeg", "Image"),
         ("gif", "Image"),
         ("bmp", "Image"),
         ("svg", "Image"),
+        ("icon", "Image"),
         ("txt", "Document"),
         ("pdf", "Document"),
         ("doc", "Document"),
@@ -64,6 +70,7 @@ fn get_extension_map() -> HashMap<String, String> {
         ("exe", "Executable"),
         ("out", "Executable"),
         ("app", "Executable"),
+        ("msi", "Executable"),
         ("deb", "Linux_Binaries"),
         ("rpm", "Linux_Binaries"),
     ];
@@ -74,36 +81,67 @@ fn get_extension_map() -> HashMap<String, String> {
         .collect()
 }
 
-pub fn run(dir: PathBuf) -> Result<(), Box<dyn std::error::Error>> {
+pub fn run(config: &Args) -> Result<(), Box<dyn std::error::Error>> {
     let map = get_extension_map();
-    let entries = std::fs::read_dir(&dir)?;
+    let entries = std::fs::read_dir(&config.dir)?;
     for entry in entries {
         let entry = entry?;
+
         let file_path = entry.path();
-        if file_path.is_file() {
-            let extension = file_path.extension().unwrap_or_default();
-            let file_type = map
-                .get(extension.to_string_lossy().to_string().trim())
-                .unwrap_or(&"Other".to_string())
-                .to_string();
-            let target_dir = dir.join(&file_type);
-            std::fs::create_dir_all(&target_dir)?;
-            let targer_path = target_dir.join(file_path.file_name().unwrap());
-            std::fs::rename(&file_path, &targer_path)?;
-            moved_message(
+
+        if !file_path.is_file() {
+            continue;
+        }
+
+        let extension = file_path.extension().unwrap_or_default();
+
+        let file_type = match &config.depth {
+            true => extension.to_string_lossy().to_string(),
+            false => match map.get(extension.to_string_lossy().to_string().trim()) {
+                Some(val) => val.to_string(),
+                None => continue,
+            },
+        };
+
+        let target_dir = &config.dir.join(&file_type);
+
+        if std::fs::create_dir_all(&target_dir).is_err() {
+            let msg = format!(
+                "failed to create directory for {}",
                 file_path
                     .file_name()
                     .unwrap_or_default()
                     .to_string_lossy()
-                    .to_string(),
-                file_type,
+                    .to_string()
             );
+            err_msg(&msg);
+            continue;
         }
+
+        let targer_path = target_dir.join(file_path.file_name().unwrap_or_default());
+
+        if std::fs::rename(&file_path, &targer_path).is_err() {
+            err_msg("Failed to move the file to categorized directory");
+            continue;
+        }
+
+        success_msg(
+            file_path
+                .file_name()
+                .unwrap_or_default()
+                .to_string_lossy()
+                .to_string(),
+            file_type,
+        );
     }
     Ok(())
 }
 
-fn moved_message(s1: String, s2: String) {
+fn err_msg(message: &str) {
+    eprintln!("{}", message.red());
+}
+
+fn success_msg(s1: String, s2: String) {
     println!(
         "{} --> {:.20} --> {}",
         "Moving...".bright_yellow(),
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use structopt::StructOpt;
 
 fn main() {
     let args = forg::Args::from_args();
-    if let Err(e) = forg::run(args.dir) {
+    if let Err(e) = forg::run(&args) {
         eprintln!("Application error: {e}");
         process::exit(1);
     }
